74d76c3785485ad495e042a2505f6366e47f4f82
Nagaraj Natarajan <nagaraj.natarajan@boeing.com>
2022-05-07 20:00:37 +0530

    Prototype Added

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DesignPatterns.Facade;
 using DesignPatterns.Factory;
+using DesignPatterns.Prototype;
 using System;
 
 namespace DesignPatterns
@@ -33,6 +34,37 @@ namespace DesignPatterns
                 creditCard.CardType, creditCard.CreditLimit, creditCard.AnnualCharge);
             Console.ReadKey();
         }
+
+        static void InvokePrototype()
+        {
+            InvitationCard obj1 = new InvitationCard();
+            obj1.p_To = "Ram";
+            obj1.p_Title = "My birthday invitation";
+            obj1.p_content = "Hey guys !! I am throwing a cheers party in my home";
+            obj1.SendBy = "Sourav";
+            obj1.p_Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            //Here our first object has created  
+            InvitationCard[] objList = new InvitationCard[5];
+            String[] nameList = { "Ram", "Shyam", "Hari", "Tapan", "Sukant" };
+            int i = 0;
+            foreach (String name in nameList)
+            {               
+                objList[i] = obj1.CloneMe(obj1);
+                objList[i].p_To = nameList[i];
+                i++;
+            }
+            // Print all Invitation Card here  
+            foreach (InvitationCard obj in objList)
+            {
+                Console.WriteLine("To :- " + obj.p_To);
+                Console.WriteLine("Title :- " + obj.p_Title);
+                Console.WriteLine("Content :- " + obj.p_content);
+                Console.WriteLine("Send By :- " + obj.p_SendBy);
+                Console.WriteLine("Date :- " + obj.Date);
+                Console.WriteLine("\n");
+            }
+        }
+
        static void InvokeFacade()
         {
             Console.WriteLine("----------------------CLIENT ORDERS FOR PIZZA----------------------------\n");
@@ -47,7 +79,8 @@ namespace DesignPatterns
         {
             //InvokeFacade();
 
-            InvokeFactory();
+            //InvokeFactory();
+            InvokePrototype();
 
             Console.ReadKey();
         }
---
+++ DesignPatterns/DesignPatterns/Prototype/InvitationCard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Prototype
+{
+    class InvitationCard
+    {
+
+        public string To;
+        public string Title;
+        public string Content;
+        public string SendBy;
+        public DateTime Date;
+        public String p_To
+        {
+            get { return To; }
+            set { To = value; }
+        }
+        public String p_Title
+        {
+            get { return Title; }
+            set { Title = value; }
+        }
+        public String p_content
+        {
+            get { return Content; }
+            set { Content = value; }
+        }
+        public String p_SendBy
+        {
+            get { return SendBy; }
+            set { SendBy = value; }
+        }
+        public DateTime p_Date
+        {
+            get { return Date; }
+            set { Date = value; }
+        }
+
+
+        public InvitationCard CloneMe(InvitationCard ob)
+        {
+            return (InvitationCard)this.MemberwiseClone();
+        }
+
+    }
+}
